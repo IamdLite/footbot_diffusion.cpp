25dbbf3c92ae473ac5823f3d3178fa878f3c8f0f
esdras fopa <70290973+IamdLite@users.noreply.github.com>
2023-10-27 00:07:15 +0100

    Update footbot_diffusion.cpp, increased the speed when the robot tries to escape an obstacle and brings it back to normal when it doesn't

--- footbot_diffusion.cpp
+++ footbot_diffusion.cpp
@@ -8,98 +8,62 @@
 /****************************************/
 /****************************************/
 
-CFootBotDiffusion::CFootBotDiffusion() :
-   m_pcWheels(NULL),
-   m_pcProximity(NULL),
-   m_cAlpha(10.0f),
-   m_fDelta(0.5f),
-   m_fWheelVelocity(2.5f),
-   m_cGoStraightAngleRange(-ToRadians(m_cAlpha),
-                           ToRadians(m_cAlpha)) {}
+class CFootBotDiffusion : public CCI_Controller {
 
-/****************************************/
-/****************************************/
+public:
 
-void CFootBotDiffusion::Init(TConfigurationNode& t_node) {
-   /*
-    * Get sensor/actuator handles
-    *
-    * The passed string (ex. "differential_steering") corresponds to the
-    * XML tag of the device whose handle we want to have. For a list of
-    * allowed values, type at the command prompt:
-    *
-    * $ argos3 -q actuators
-    *
-    * to have a list of all the possible actuators, or
-    *
-    * $ argos3 -q sensors
-    *
-    * to have a list of all the possible sensors.
-    *
-    * NOTE: ARGoS creates and initializes actuators and sensors
-    * internally, on the basis of the lists provided the configuration
-    * file at the <controllers><footbot_diffusion><actuators> and
-    * <controllers><footbot_diffusion><sensors> sections. If you forgot to
-    * list a device in the XML and then you request it here, an error
-    * occurs.
-    */
-   m_pcWheels    = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
-   m_pcProximity = GetSensor  <CCI_FootBotProximitySensor      >("footbot_proximity"    );
-   /*
-    * Parse the configuration file
-    *
-    * The user defines this part. Here, the algorithm accepts three
-    * parameters and it's nice to put them in the config file so we don't
-    * have to recompile if we want to try other settings.
-    */
-   GetNodeAttributeOrDefault(t_node, "alpha", m_cAlpha, m_cAlpha);
-   m_cGoStraightAngleRange.Set(-ToRadians(m_cAlpha), ToRadians(m_cAlpha));
-   GetNodeAttributeOrDefault(t_node, "delta", m_fDelta, m_fDelta);
-   GetNodeAttributeOrDefault(t_node, "velocity", m_fWheelVelocity, m_fWheelVelocity);
-}
+   CFootBotDiffusion() :
+      m_pcWheels(NULL),
+      m_pcProximity(NULL),
+      m_cAlpha(10.0f),
+      m_fDelta(0.5f),
+      m_fNormalSpeed(2.5f),  // Set your normal speed here
+      m_fEscapeSpeed(5.0f),  // Set your escape speed here
+      m_cGoStraightAngleRange(-ToRadians(m_cAlpha),
+                              ToRadians(m_cAlpha)) {}
 
-/****************************************/
-/****************************************/
-
-void CFootBotDiffusion::ControlStep() {
-   /* Get readings from proximity sensor */
-   const CCI_FootBotProximitySensor::TReadings& tProxReads = m_pcProximity->GetReadings();
-   /* Sum them together */
-   CVector2 cAccumulator;
-   for(size_t i = 0; i < tProxReads.size(); ++i) {
-      cAccumulator += CVector2(tProxReads[i].Value, tProxReads[i].Angle);
+   virtual void Init(TConfigurationNode& t_node) {
+      m_pcWheels    = GetActuator<CCI_DifferentialSteeringActuator>("differential_steering");
+      m_pcProximity = GetSensor  <CCI_FootBotProximitySensor      >("footbot_proximity");
+      GetNodeAttributeOrDefault(t_node, "alpha", m_cAlpha, m_cAlpha);
+      m_cGoStraightAngleRange.Set(-ToRadians(m_cAlpha), ToRadians(m_cAlpha));
+      GetNodeAttributeOrDefault(t_node, "delta", m_fDelta, m_fDelta);
    }
-   cAccumulator /= tProxReads.size();
-   /* If an obstacle is detected, perform obstacle avoidance */
-   if (cAccumulator.Length() > m_fDelta) {
-      /* Calculate the avoidance direction */
-      CRadians cAngle = cAccumulator.Angle();
-      if (cAngle.GetValue() > 0.0f) {
-         /* Turn left */
-         m_pcWheels->SetLinearVelocity(m_fWheelVelocity, 0.0f);
+
+   virtual void ControlStep() {
+      /* Get readings from proximity sensor */
+      const CCI_FootBotProximitySensor::TReadings& tProxReads = m_pcProximity->GetReadings();
+      /* Sum them together */
+      CVector2 cAccumulator;
+      for(size_t i = 0; i < tProxReads.size(); ++i) {
+         cAccumulator += CVector2(tProxReads[i].Value, tProxReads[i].Angle);
+      }
+      cAccumulator /= tProxReads.size();
+      /* Check if an obstacle is detected */
+      if (cAccumulator.Length() > m_fDelta) {
+         /* Obstacle detected, set escape speed */
+         m_pcWheels->SetLinearVelocity(m_fEscapeSpeed, m_fEscapeSpeed);
       }
       else {
-         /* Turn right */
-         m_pcWheels->SetLinearVelocity(0.0f, m_fWheelVelocity);
+         /* No obstacle detected, set normal speed */
+         m_pcWheels->SetLinearVelocity(m_fNormalSpeed, m_fNormalSpeed);
       }
    }
-   else {
-      /* No obstacle detected, go straight */
-      m_pcWheels->SetLinearVelocity(m_fWheelVelocity, m_fWheelVelocity);
-   }
-}
+
+   virtual void Reset() {}
+
+private:
+
+   CCI_DifferentialSteeringActuator* m_pcWheels;
+   CCI_FootBotProximitySensor* m_pcProximity;
+   CRadians m_cAlpha;
+   Real m_fDelta;
+   Real m_fNormalSpeed;
+   Real m_fEscapeSpeed;
+   CRange<CRadians> m_cGoStraightAngleRange;
+};
 
 /****************************************/
 /****************************************/
 
-/*
- * This statement notifies ARGoS of the existence of the controller.
- * It binds the class passed as first argument to the string passed as
- * second argument.
- * The string is then usable in the configuration file to refer to this
- * controller.
- * When ARGoS reads that string in the configuration file, it knows which
- * controller class to instantiate.
- * See also the configuration files for an example of how this is used.
- */
 REGISTER_CONTROLLER(CFootBotDiffusion, "footbot_diffusion_controller")
